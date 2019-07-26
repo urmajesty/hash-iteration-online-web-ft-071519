6002f878fb9744668decf3e9470b0f9c7d37bf6d
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-26 14:26:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,3 +15,8 @@ def select_winner(passengers)
  end
   winner
 end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age
+  puts "Happy Birthday #{kids_name}!  You are now #{age} years old!"
+end
